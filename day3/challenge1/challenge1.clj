d4f9d407fac9911d1c2bc3fdebedc3f7cf0f7f6b
Pat Viafore <patviafore@gmail.com>
2016-12-05 19:49:32 -0600

    Uploading solution for day 3, part 1

---
+++ day3/challenge1/challenge1.clj
@@ -0,0 +1,29 @@
+(defn read-file [] (slurp "../day3.txt"))
+(defn split-lines [str] (clojure.string/split str #"\n"))
+
+
+(defn make-triangle [string] (->>
+                            (clojure.string/split string #" ")
+                            (remove #(= % "" ))
+                            (map read-string)
+                            (sort)
+                            (seq)
+                            )
+)
+
+(defn make-triangles [strings] (map make-triangle strings))
+
+(defn is-valid-triangle [[a b c]] (> (+ a b) c))
+(defn get-valid-triangles [triangles] (filter is-valid-triangle triangles))
+
+(defn main [] (->
+               (read-file)
+               (split-lines)
+               (make-triangles)
+               (get-valid-triangles)
+               (count)
+              )
+)
+
+(main)
+               
