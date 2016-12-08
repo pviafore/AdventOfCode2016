2354e86b1e34ee793f7f98c08e4b8b3f889fb946
Pat Viafore <patviafore@gmail.com>
2016-12-07 22:27:45 -0600

    Fixing challenge 2 function name

--- day6/challenge2/challenge2.jl
+++ day6/challenge2/challenge2.jl
@@ -1,4 +1,4 @@
-function getHighestCount(chars)
+function getLowestCount(chars)
     dict = Dict()
     for char in chars
        dict[char] = get(dict, char, 0) + 1
@@ -10,7 +10,7 @@ f = open("../day6.txt")
     lines = readlines(f)
     chars = map(s -> split(strip(s), ""), lines)
     reshaped = hcat(chars...)
-    columns = map(x -> getHighestCount(reshaped[x,:]), 1:length(reshaped[:,1]))
+    columns = map(x -> getLowestCount(reshaped[x,:]), 1:length(reshaped[:,1]))
     answer = *(map(c -> c[1], columns)...)
     println(answer)
 close(f)
