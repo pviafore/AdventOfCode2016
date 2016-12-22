905b5c027cf9ef4b01af422329cae32a4d7a6cbd
Pat Viafore <patviafore@gmail.com>
2016-12-21 20:22:36 -0600

    Finishing day 1

--- day16/challenge1/Challenge1.scala
+++ day16/challenge1/Challenge1.scala
@@ -1,6 +1,7 @@
 object Challenge1 {
 
   val input = "01000100010010111"
+  val diskSize = 272
   def invertByte(c: Char): Char = {
       if (c == '0'){
          '1'
@@ -18,12 +19,40 @@ object Challenge1 {
       s + "0" + invert(s reverse)
   }
 
-  val dragonStream: Stream[String] = {
-      def loop(s: String): Stream[String] = s #:: loop(dragonize(s))
-      loop(input)
+  def getDragon(input: String, maxSize: Int) : String = {
+      if (input.length >= maxSize) {
+          return input.slice(0, maxSize)
+      }
+      else {
+          return getDragon(dragonize(input), maxSize)
+      }
   }
 
+  def reduce(s: String): String = {
+      s.slice(1,4)
+  }
+
+  def condenseBytes(c1: Char, c2: Char): Char = {
+      if (c1 == c2) '1' else '0'
+  }
+
+  def getChecksum(s: String):String = {
+      if (s.length % 2 == 1) {
+          s
+      }
+      else{
+          val next:String = s.grouped(2).map(s => condenseBytes(s.charAt(0), s.charAt(1))).mkString
+          getChecksum(next)
+      }
+  }
+
+
+
+
   def main(args: Array[String]): Unit = {
-    dragonStream take 3 foreach println
+    val s:String = getDragon(input, diskSize)
+    val checksum:String = getChecksum(s)
+    println(checksum)
+    
   }
 }
