f814aa8b701266efa8049e5223eaec2fbf55d0ef
Pat Viafore <patviafore@gmail.com>
2016-12-22 21:27:35 -0600

    Bash for day 18 challenge1

---
+++ day18/challenge1/challenge1.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+target=$(<"../day18.txt")
+
+getChar() {
+    sequence=$1
+    index=$2
+    if [ "$index" == "-1" ]; then
+        char="."
+    elif [ "$index" == "${#sequence}" ]; then
+        char="."
+    else
+        char=${sequence:$index:1}
+    fi
+}
+getNextChar() {
+    getChar $1 $(($2-1))
+    char1=$char
+    getChar $1 $2
+    char2=$char
+    getChar $1 $(($2+1))
+    char3=$char
+    if [[ "$char1" = "^" && "$char2" = "^" && "$char3" = "." ]]; then
+        nextChar="^";
+    elif [[ "$char1" = "." && "$char2" = "^" && "$char3" = "^" ]]; then
+        nextChar="^";
+    elif [[ "$char2" = "." && "$char1" = "." && "$char3" = "^" ]]; then
+        nextChar="^";
+    elif [[ "$char2" = "." && "$char3" = "." && "$char1" = "^" ]]; then
+        nextChar="^";
+    else   
+         nextChar=".";
+    fi
+}
+
+getNextSequence() {
+    sequence=$1
+    target=""
+    for (( i=0; i<${#sequence}; i++)) 
+    do
+        getNextChar $sequence $i
+        target=$target$nextChar
+    done
+}
+
+rows=40
+fullString=""
+for (( j=0; j<${rows}; j++))
+do
+    fullString=$fullString$target
+    getNextSequence $target    
+done
+safe="${fullString//[^.]}"
+echo ${#safe}
